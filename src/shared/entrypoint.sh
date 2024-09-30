2a257922d2018379d5c10247ed1c37ae90498b29
marcel ndeffo <47064690+MarcelNasser@users.noreply.github.com>
2024-09-30 08:20:01 +0200

    add gcs filesystem support (#10)

--- src/shared/entrypoint.sh
+++ src/shared/entrypoint.sh
@@ -15,25 +15,32 @@ root_dir=$(realpath "$(dirname "$0")")
 [ -z "$HOST" ] && error "HOST is not provided."
 [ -z "$PORT" ] && error "PORT is not provided."
 
-#local disk
+#local db
 if [ -z "$DB" ]; then
   echo "x no remote storage.."
   mlflow server  \
-    --host "$HOST" --port "$PORT" --gunicorn-opts "--log-level debug"
-fi
+    --host "$HOST" --port "$PORT" --gunicorn-opts "--log-level debug" && exit 0 || exit 1
 
 #S3
-if [ -n "$MLFLOW_S3_ENDPOINT_URL" -a -n "$S3" ]; then
+elif [ -n "$MLFLOW_S3_ENDPOINT_URL" -a -n "$S3" ]; then
    echo "+ remote fs: s3"
-   "$root_dir/bucket.py" && \
+   "$root_dir/check_bucket_s3.py" && \
    mlflow server  \
             --host "$HOST" --port "$PORT" \
             --backend-store-uri "$DB" \
             --artifacts-destination "$S3" --gunicorn-opts "--log-level debug" && exit 0 || exit 1
-fi
+
+#GCS
+elif [ -n "$GCS" ]; then
+   echo "+ remote fs: gcs"
+   "$root_dir/check_bucket_gcs.py" && \
+   mlflow server  \
+            --host "$HOST" --port "$PORT" \
+            --backend-store-uri "$DB" \
+            --artifacts-destination "$GCS" --gunicorn-opts "--log-level debug" && exit 0 || exit 1
 
 #nfs
-if [ -n "$NFS" -a -n "$STORE" ]; then
+elif [ -n "$NFS" -a -n "$STORE" ]; then
   echo "+ remote fs: nfs"
   mkdir -p "$STORE";
   mkdir -p /run/sendsigs.omit.d/;
@@ -44,10 +51,11 @@ if [ -n "$NFS" -a -n "$STORE" ]; then
   mlflow server --backend-store-uri "$DB" \
                 --artifacts-destination file://"$STORE" \
                 --host "$HOST" --port "$PORT" --gunicorn-opts "--log-level debug" && exit 0 || exit 1
-fi
 
-#finally
-echo "x no remote fs"
-mlflow server  \
-    --host "$HOST" --port "$PORT" \
-    --backend-store-uri "$DB" --gunicorn-opts "--log-level debug"
+#local fs
+else
+  echo "x no remote fs"
+  mlflow server  \
+      --host "$HOST" --port "$PORT" \
+      --backend-store-uri "$DB" --gunicorn-opts "--log-level debug" && exit 0 || exit 1
+fi
